3304678263069dff3a266f4e766b904b8cc8a8b9
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-18 16:04:57 +0600

    about-section

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -4,27 +4,32 @@ const About = () => {
     return (
         <div id='about' className='merina mid-lg:w-4/5 py-24'>
             <h1 className='TitleText font-bold text-[#ccd6f6]'>About Me</h1>
-            <div className='mt-14'>
+            <div className='mt-14 font-Calibre text-justify'>
                 <p>
                     Hey there, I'm Tanjimul Islam Sabbir, a front-end developer with a love for turning code into amazing web experiences. In the vibrant year of 2021, a transformative chapter began as I stepped into the halls of Rajshahi University, setting the stage for an extraordinary odyssey. I got my first own computer and soon found myself falling in love with coding. Python was my first language, and it opened the door to a world of my programmimg.
                 </p>
                 <br />
                 <p>
-                    As time passed, I got into web technologies – things like HTML, CSS, and Bootstrap. But it was when I discovered tools like Tailwind CSS, Material UI, and JavaScript that my real passion ignited. React.js became my playground, and Redux and GraphQL joined the mix for smoother operations.
-                </p>
-                <br />
-                <p>
-                    While I've explored backend technologies like Node.js, Express.js, and MongoDB, my heart truly belongs to the front end. I thrive on solving intricate problems and creating interactive user experiences. Design is cool, but I find my joy in making things work seamlessly.
-                </p>
-                <br />
-                <p>
-                    I recently had an opportunity to work with a great company in India. Although I had to step back due to my university's class and exam, the experience taught me a lot and fueled my commitment to learning and growth.
+                    <strong className='text-green-500'> As time passed,</strong>  I got into web technologies – things like HTML, CSS, and Bootstrap. But it was when I discovered tools like Tailwind CSS, Material UI, and JavaScript that my real passion ignited. React.js became my playground, and Redux and GraphQL joined the mix for smoother operations.
                 </p>
 
                 <p>
-                    Collaboration is key for me. I believe in the power of teamwork, where every voice matters. Outside of coding, I enjoy nature photgrapy and music, which adds a dash of inspiration to my work.
+                    <strong className='text-green-500'> While I've explored backend technologies like Node.js,</strong>
+                    Express.js, and MongoDB, my heart truly belongs to the front end. I thrive on solving intricate problems and creating interactive user experiences. Design is cool, but I find my joy in making things work seamlessly. <br />
+                    I recently had an opportunity to work with a great company in India. Although I had to step back due to my university's class and exam, the experience taught me a lot and fueled my commitment to learning and growth. Collaboration is key for me. I believe in the power of teamwork, where every voice matters.
                 </p>
-                Here's to coding, creativity, and my shared journey!
+                <p>
+                    <strong className='text-green-500'>Outside of coding,</strong>
+
+                    I enjoy nature photograpy and music, which adds a dash of inspiration to my work.
+                    MY Favourite Colour is Skyblue.
+                    Gardening is
+                    my hobby. I have an indoor
+                    garden. I love nature. In my leisure period, I collect various kinds of Nature Photos. My favourite
+                    flower is
+                    Water-hyacinths. It is a polyphyletic genus of the aquatic flowering plants.
+                </p>
+                <p>  <strong className='text-green-500'>Here's to coding, </strong>  creativity, and my shared journey!</p>
             </div>
         </div>
     )

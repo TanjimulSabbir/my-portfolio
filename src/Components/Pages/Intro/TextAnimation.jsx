fa6f9e387979d105385995368b171f5eb75abadb
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-20 04:14:14 +0600

    adding style on text

--- src/Components/Pages/Intro/TextAnimation.jsx
+++ src/Components/Pages/Intro/TextAnimation.jsx
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import "./TextAnimationCss.css"
 
 const Typewriter = () => {
   const text = `I'm a skilled front-end developer with a passion for building dynamic and interactive websites. My expertise lies in harnessing the power of Javascript, React.js, Tailwind CSS, Redux, and other cutting-edge technologies to craft seamless web experiences that engage and captivate users.`;
 
   const [typedText, setTypedText] = useState('');
   const [index, setIndex] = useState(0);
-  const [completed, setCompleted] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     const typingInterval = setInterval(() => {
-      if (index <= text.length && !completed && !isPaused) {
+      if (index <= text.length) {
         setTypedText(text.substring(0, index));
         setIndex(prevIndex => prevIndex + 1);
-      } else if (index === text.length + 1) {
-        setCompleted(true);
+      } else {
         clearInterval(typingInterval);
         setTimeout(() => {
           setTypedText('');
           setIndex(0);
-          setCompleted(false);
         }, 3000); // Delay before starting the animation again
       }
     }, 100); // Delay between each character
@@ -27,21 +24,31 @@ const Typewriter = () => {
     return () => {
       clearInterval(typingInterval);
     };
-  }, [index, text, completed, isPaused]);
-
-  const handlePauseResume = () => {
-    setIsPaused(!isPaused);
-  };
+  }, [index, text]);
 
   return (
     <div className="my-20 typewriter">
       <p className="lg:w-2/3 mx-auto text-center">
-        <span className="text-animate">{typedText}</span>
+        <span className="text-animate">
+          <span className="skew-animate">
+            {typedText.split('').map((char, idx) => {
+              if (idx >= 13 && idx <= 32) { // 'front-end developer'
+                return <span key={idx} className="highlight-letter">{char}</span>;
+              } else if (idx >= 6 && idx <= 12) { // 'skilled'
+                return <span key={idx} className="red-letter">{char}</span>;
+              } else if (idx >= 86 && idx <= 90) { // 'power'
+                return <span key={idx} className="red-letter">{char}</span>;
+              } else if (idx >= 28 && idx <= 34) { // 'dynamic'
+                return <span key={idx} className="bold-letter">{char}</span>;
+              } else if (idx >= 119 && idx <= 141) { // 'cutting-edge technologies'
+                return <span key={idx} className="bold-letter">{char}</span>;
+              }
+              return char;
+            })}
+          </span>
+        </span>
         <span className="cursor">{index <= text.length ? '|' : ''}</span>
       </p>
-      <button onClick={handlePauseResume}>
-        {isPaused ? 'Resume' : 'Pause'}
-      </button>
     </div>
   );
 };

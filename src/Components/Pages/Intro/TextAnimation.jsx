5ccc0ec2910d22013938719e862cc43290d4aed7
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-20 04:43:57 +0600

    fron-end-text=colored

--- src/Components/Pages/Intro/TextAnimation.jsx
+++ src/Components/Pages/Intro/TextAnimation.jsx
@@ -32,28 +32,67 @@ const Typewriter = () => {
         <span className="text-animate">
           <span className="skew-animate">
             {typedText.split('').map((char, idx) => {
-              if (idx >= 13 && idx <= 32) { // 'front-end developer'
-                return <span key={idx} className="highlight-letter">{char}</span>;
-              } else if (idx >= 6 && idx <= 12) { // 'skilled'
-                return <span key={idx} className="red-letter">{char}</span>;
-              } else if (idx >= 86 && idx <= 90) { // 'power'
-                return <span key={idx} className="red-letter">{char}</span>;
-              } else if (idx >= 28 && idx <= 34) { // 'dynamic'
-                return <span key={idx} className="bold-letter">{char}</span>;
-              } else if (idx >= 119 && idx <= 141) { // 'cutting-edge technologies'
-                return <span key={idx} className="bold-letter">{char}</span>;
-              } else if (idx >= 56 && idx <= 61) { // 'passion'
-                return <span key={idx} className="blue-letter">{char}</span>;
-              } else if (idx >= 95 && idx <= 104) { // 'interactive'
-                return <span key={idx} className="underline-letter">{char}</span>;
-              } else if (idx >= 35 && idx <= 40) { // 'engage'
-                return <span key={idx} className="rainbow-letter">{char}</span>;
-              } else if (idx >= 41 && idx <= 47) { // 'captivate'
-                return <span key={idx} className="rotate-letter">{char}</span>;
-              } else if (idx >= 136 && idx <= 142) { // 'users.'
-                return <span key={idx} className="glow-letter">{char}</span>;
+              const styles = [];
+              if (idx >= 13 && idx <= 32) {
+                styles.push("highlight-letter");
               }
-              return char;
+              if (idx === 50) {
+                styles.push("green-letter");
+              }
+              if (idx === 87) {
+                styles.push("red-letter");
+              }
+              if (idx >= 98 && idx <= 108) {
+                styles.push("blue-letter");
+              }
+              if (idx >= 115 && idx <= 126) {
+                styles.push("yellow-letter");
+              }
+              if (idx >= 54 && idx <= 59) {
+                styles.push("blue-bold-letter");
+              }
+              if (idx >= 4 && idx <= 14) {
+                styles.push("underline-letter");
+              }
+              if (idx >= 94 && idx <= 104) {
+                styles.push("rainbow-letter");
+              }
+              if (idx >= 81 && idx <= 85) {
+                styles.push("glow-letter");
+              }
+              if (idx >= 88 && idx <= 105) {
+                styles.push("highlight-letter");
+              } if (idx >= 114 && idx <= 139) {
+                styles.push("highlight-letter");
+              } if (idx >= 150 && idx <= 176) {
+                styles.push("highlight-letter");
+              }
+              if (idx >= 189 && idx <= 200) {
+                styles.push("highlight-letter");
+              }
+
+              if (idx >= 210 && idx <= 217) {
+                styles.push("highlight-letter");
+              }
+
+              if (idx >= 235 && idx <= 246) {
+                styles.push("highlight-letter");
+              }
+
+              if (idx >= 269 && idx <= 278) {
+                styles.push("highlight-letter");
+              }
+
+              if (idx >= 280 && idx <= 296) {
+                styles.push("highlight-letter");
+              }
+
+
+              if (styles.length > 0) {
+                return <span key={idx} className={styles.join(' ')}>{char}</span>;
+              }
+              
+              return <span key={idx}>{char}</span>;
             })}
           </span>
         </span>

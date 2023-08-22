99e1bbf7db92e948d594cb3d2074d29edb992628
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-22 23:14:19 +0600

    working-on-project-page

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -1,22 +1,23 @@
 
-import { IoLinkOutline } from "react-icons/io5";
+import { LuExternalLink } from "react-icons/lu";
 import { FiGithub } from "react-icons/fi";
 const Work = () => {
     return (
         <div id='work' className='mid-lg:w-4/5 py-24'>
-            <h1 className='TitleText font-bold text-[#ccd6f6] mb-14'>Some Things I've built<span className="Section-Heading"></span></h1>
+            <h1 className='Exo2_SanSer TitleText font-bold text-[#ccd6f6] mb-14'>Some Things I've built<span className="Section-Heading"></span></h1>
             <div className="mid-lg:flex">
                 <div className="flex-1">
                     <img src="" alt="" srcset="" />
                 </div>
                 <div className="flex-1">
                     <h2 className="mb-2 text-custom-highLightTextColor">Featured Project</h2>
-                    <h1 className="mb-8 text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor">Paradise Palam</h1>
-                    <p className="mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
-                    <p><small></small></p>
-                    <div className="flex space-x-3 text-2xl">
+                    <h1 className="mb-8 text-custom-TitleColor
+                    text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor Exo2_SanSer">Paradise Palam</h1>
+                    <p className="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
+                    <p className="mb-4"><small>VS Code</small></p>
+                    <div className="flex items-center space-x-8 text-xl">
                       <a href="/"><FiGithub /> </a>
-                       <a href=""> <IoLinkOutline /></a>
+                       <a className="" href=""> <LuExternalLink /></a>
                     </div>
                 </div>
             </div>

9468a4035556a253e350cd4c2970b81fc15b49e4
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-22 22:59:25 +0600

    working-on-project-page

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -5,18 +5,18 @@ const Work = () => {
     return (
         <div id='work' className='mid-lg:w-4/5 py-24'>
             <h1 className='TitleText font-bold text-[#ccd6f6] mb-14'>Some Things I've built<span className="Section-Heading"></span></h1>
-            <div className="flex">
+            <div className="mid-lg:flex">
                 <div className="flex-1">
                     <img src="" alt="" srcset="" />
                 </div>
                 <div className="flex-1">
-                    <h2>Featured Project</h2>
-                    <h1>Paradise Palam</h1>
+                    <h2 className="mb-4">Featured Project</h2>
+                    <h1 className="mb-5">Paradise Palam</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
                     <p><small></small></p>
-                    <div>
-                        <FiGithub />
-                        <IoLinkOutline />
+                    <div className="flex space-x-3">
+                      <a href="/"><FiGithub /> </a>
+                       <a href=""> <IoLinkOutline /></a>
                     </div>
                 </div>
             </div>

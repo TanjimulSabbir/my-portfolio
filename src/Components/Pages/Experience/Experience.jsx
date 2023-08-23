90c52edcf8e0d509ff7c6d509e4b21dd46615072
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 03:09:02 +0600

    experience-page-desiging-responsive

--- src/Components/Pages/Experience/Experience.jsx
+++ src/Components/Pages/Experience/Experience.jsx
@@ -5,24 +5,34 @@ const Experience = () => {
         <div id='experience' className='mid-lg:w-3/5 py-24 mx-auto'>
             {/* Heading */}
             <h1 className='Exo2_SanSer TitleText font-bold text-[#ccd6f6] mb-14'>Where I'v worked<span className="Section-Heading"></span></h1>
-            <div className='mt-14 flex space-x-20 justify-center'>
+            <div className='mt-14 flex space-x-4 md:space-x-20 justify-center'>
                 <div className=''>
-                    <div className="text-custom-highLightTextColor">
+                    <div className="text-custom-highLightTextColor text-base">
                         <h1>Inventive</h1>
                     </div>
                 </div>
                 <div>
-                    <div className="border-l border-custom-highLightTextColor border-spacing-1 pl-4">
-                        <div className="flex space-x-3">
+                    <div className="border-l border-custom-highLightTextColor pl-4">
+                        <div className="flex flex-col sm:items-center sm:flex-row sm:space-x-3">
                             <h1>Front-end Developer</h1>
-                            <p className="text-custom-highLightTextColor">@Inventive Web Service</p>
+                            <p className="text-custom-highLightTextColor mb-4 sm:mb-0 text-[10px] sm:text-base">@Inventive Web Service</p>
                         </div>
                         <div className="">
                             <small>May 2023 - July 2023</small>
                         </div>
-                        <div className="flex space-x-2 mt-5">
-                            <VscDebugBreakpointDataUnverified className="text-xl w-10 text-custom-highLightTextColor" />
-                            <p className="text-custom-TextColor text-sm">   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aut ratione asperiores beatae perspiciatis rerum nostrum laboriosam ad tempora tempore?</p>
+                        <div className="space-y-3 mt-5">
+                            <div className="flex space-x-2">
+                                <VscDebugBreakpointDataUnverified className="text-xl w-10 text-custom-highLightTextColor" />
+                                <p className="text-custom-TextColor text-sm">   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aut ratione asperiores beatae perspiciatis rerum nostrum laboriosam ad tempora tempore?</p>
+                            </div>
+                            <div className="flex space-x-2">
+                                <VscDebugBreakpointDataUnverified className="text-xl w-10 text-custom-highLightTextColor" />
+                                <p className="text-custom-TextColor text-sm">   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aut ratione asperiores beatae perspiciatis rerum nostrum laboriosam ad tempora tempore?</p>
+                            </div>
+                            <div className="flex space-x-2">
+                                <VscDebugBreakpointDataUnverified className="text-xl w-10 text-custom-highLightTextColor" />
+                                <p className="text-custom-TextColor text-sm">   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum aut ratione asperiores beatae perspiciatis rerum nostrum laboriosam ad tempora tempore?</p>
+                            </div>
                         </div>
                     </div>
                 </div>

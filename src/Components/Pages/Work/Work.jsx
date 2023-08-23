21e44ddad37421fe58c74e532f54e4e29913d81f
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 00:14:55 +0600

    project-page-responsive-desiging-bg-hover-set

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -11,27 +11,28 @@ const Work = () => {
             {/* Projects */}
 
             {/* image part */}
-            <div className="grid grid-cols-2 inset-0 relative">
+            <div className="relative">
                 <div className="">
-                    <div className="w-[500px] h-[400px] relative">
+                    <div className="w-full h-full lg:w-[500px] lg:h-[400px] relative">
                         {/* Background image */}
-                        <div className="bg-cover bg-center rounded"
+                        {/* <div className="bg-cover bg-center rounded"
                             style={{ backgroundImage: `url(${backgroundImage})`, height: '100%' }}>
-                        </div>
-                        <div className="absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-green-500 opacity-30 hover:opacity-0"></div>
+                        </div> */}
+                        <img className="" src={backgroundImage} alt="" srcset="" />
+                        <div className=" absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-[#0d2446] lg:bg-green-500 bg-opacity-90 lg:bg-opacity-30 hover:opacity-0"></div>
                     </div>
                 </div>
                 {/* Text Part */}
                 <div className="">
-                    <div className="absolute inset-0 text-end flex items-center justify-end">
-                        <div className="mr-0 max-w-[60%]">
+                    <div className="absolute inset-0 lg:text-end flex items-center justify-end p-4 lg:p-0">
+                        <div className="mr-0 lg:max-w-[60%]">
                             <h2 className="mb-2 text-custom-highLightTextColor">Featured Project</h2>
                             <h1 className="mb-8 text-custom-TitleColor text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor Exo2_SanSer">Paradise Palam</h1>
 
-                            <p className="mb-10 bg-[#0d2446] text-sm py-5 px-8 rounded text-custom-TextColor shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
+                            <p className="mb-10 lg:bg-[#0d2446] text-sm py-5 lg:px-8 rounded text-custom-TextColor lg:shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
 
                             <p className="mb-4"><small>Reactjs</small> <small>Javascript</small> <small>TailwindCss</small> <small>Redux</small></p>
-                            <div className="flex items-center space-x-8 text-xl justify-end">
+                            <div className="flex items-center space-x-8 text-xl lg:justify-end">
                                 <a href="/"><FiGithub /> </a>
                                 <a className="" href=""> <LuExternalLink /></a>
                             </div>

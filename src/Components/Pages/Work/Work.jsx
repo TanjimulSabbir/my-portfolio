92f22b7df93ece1e194fa6d27ed58e0b5500bda7
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 00:35:41 +0600

    project-page-responsive-desiging-bg-hover-set

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -19,17 +19,17 @@ const Work = () => {
                             style={{ backgroundImage: `url(${backgroundImage})`, height: '100%' }}>
                         </div> */}
                         <img className="" src={backgroundImage} alt="" srcset="" />
-                        <div className=" absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-[#0d2446] lg:bg-green-500 bg-opacity-90 lg:bg-opacity-30 hover:opacity-0"></div>
+                        <div className=" absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-[#0a1424] lg:bg-green-500 bg-opacity-90 lg:bg-opacity-30 hover:opacity-0"></div>
                     </div>
                 </div>
                 {/* Text Part */}
                 <div className="">
                     <div className="absolute inset-0 lg:text-end flex items-center justify-end p-4 lg:p-0">
                         <div className="mr-0 lg:max-w-[60%]">
-                            <h2 className="mb-2 text-custom-highLightTextColor">Featured Project</h2>
+                            <h2 className="mb-2 text-custom-highLightTextColor cursor-pointer">Featured Project</h2>
                             <h1 className="mb-8 text-custom-TitleColor text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor Exo2_SanSer">Paradise Palam</h1>
 
-                            <p className="mb-10 lg:bg-[#0d2446] text-sm py-5 lg:px-8 rounded text-custom-TextColor lg:shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
+                            <p className="mb-10 lg:bg-[#0d2446] text-sm lg:py-5 lg:px-8 rounded text-custom-TitleColor lg:text-custom-TextColor  lg:shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
 
                             <p className="mb-4"><small>Reactjs</small> <small>Javascript</small> <small>TailwindCss</small> <small>Redux</small></p>
                             <div className="flex items-center space-x-8 text-xl lg:justify-end">

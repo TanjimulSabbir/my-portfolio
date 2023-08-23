7fcc6b527121d11d5b55163876990d0766104e4c
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 00:57:21 +0600

    project-page-cut-absolute-text-part-lg-mid-lg-convert

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -4,7 +4,7 @@ import { FiGithub } from "react-icons/fi";
 const Work = () => {
     const backgroundImage = 'https://i.ibb.co/mzRSWTb/bed06.jpg';
     return (
-        <div id='work' className='mid-lg:w-5/6 py-24 mx-auto'>
+        <div id='work' className='lg:w-5/6 py-24 mx-auto'>
             {/* Heading */}
             <h1 className='Exo2_SanSer TitleText font-bold text-[#ccd6f6] mb-14'>Some Things I've built<span className="Section-Heading"></span></h1>
 
@@ -12,12 +12,12 @@ const Work = () => {
             <div className="mid-lg:grid grid-cols-2 relative">
                 {/* image part */}
                 <div className="">
-                    <div className="relative">
+                    <div className="relative w-full h-full mid-lg:w-full mid-lg:h-[400px]">
                         {/* Background image */}
                         {/* <div className="bg-cover bg-center rounded"
                             style={{ backgroundImage: `url(${backgroundImage})`, height: '100%' }}>
                         </div> */}
-                        <img className="w-full h-full mid-lg:w-[500px] mid-lg:h-[400px]" src={backgroundImage} alt="" srcset="" />
+                        <img className="w-full h-full mid-lg:w-full mid-lg:h-[400px] rounded" src={backgroundImage} alt="" srcset="" />
                         <div className="absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-[#0d2446] mid-lg:bg-green-500 bg-opacity-90 mid-lg:bg-opacity-30 hover:opacity-0"></div>
                     </div>
                 </div>
@@ -28,7 +28,7 @@ const Work = () => {
                             <h2 className="mb-2 text-custom-highLightTextColor">Featured Project</h2>
                             <h1 className="mb-8 text-custom-TitleColor text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor Exo2_SanSer">Paradise Palam</h1>
 
-                            <p className="mb-10 mid-lg:bg-[#0d2446] text-base mid-lg:text-sm mid-lg:py-5 mid-lg:px-8 rounded text-custom-TextColor mid-lg:shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
+                            <p className="mb-10 mid-lg:bg-[#0d2446] text-base mid-lg:text-sm mid-lg:p-4 lg:py-5 lg:px-8 rounded text-custom-TextColor mid-lg:shadow-2xl z-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis provident molestias a in quae placeat molestiae similique laudantium esse consequuntur eaque dolores accusamus, fugiat repellendus inventore dolorem quasi nisi veritatis!</p>
 
                             <p className="mb-4"><small>Reactjs</small> <small>Javascript</small> <small>TailwindCss</small> <small>Redux</small></p>
                             <div className="flex items-center space-x-8 text-xl mid-lg:justify-end">

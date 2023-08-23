c2d593ea9cb8a7dc8b7863f8239e3a044c506a1a
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-23 17:27:22 +0600

    small-device-navbar-designing

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -2,32 +2,28 @@ import { RxHamburgerMenu } from "react-icons/rx"
 const Navbar = () => {
     const Menu = <>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
+        {/* <li><a href="#experience">Experience</a></li> */}
         <li><a href="#work">Works</a></li>
         <li><a href="#contact">Contact</a></li>
         <li><a href="#contact">Blog</a></li>
-        <li className="text-emerald-500">
-            <a href="#Resume" className='border-emerald-500'>Resume</a>
-        </li>
+        <li><a href="#Resume" >Resume</a></li>
     </>
 
     return (
-        <div className="navbar py-4 bg-[#EF412D]">
-            <div className="navbar flex item-center justify-end">
-                <div className="">
-                    <label tabIndex={0} className="btn btn-outline UniversalMargin text-4xl text-black border-none hover:bg-transparent mid-lg:hidden">
+        <div className="navbar py-4 bg-white mid-lg:bg-[#EF412D]">
+            <div className="navbar flex item-center justify-end text-black z-50">
+                <div className="mid-lg:hidden">
+                    <label tabIndex={0} className="btn btn-outline UniversalMargin text-4xl border-none hover:bg-transparent text-black mid-lg:hidden">
                       <RxHamburgerMenu/>
                     </label>
-                    <ul tabIndex={0} className="fixed inset-0 mt-20 flex items-center shadow w-screen h-screen z-50 space-y-5 bg-black bg-opacity-70 uppercase Exo2_SanSer">
-                        <>
+                    <ul tabIndex={0} className="fixed inset-0 mt-20 px-6 py-10 shadow w-screen h-screen space-y-8 bg-white uppercase">
                         {Menu}
-                        </>
                     </ul>
                 </div>
                 {/* <a href='#Tanjimul' className="text-lg">Tanjimul Sabbir</a> */}
             </div>
             <div className="navbar-center lg:navbar-end hidden mid-lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 uppercase">
                     {Menu}
                 </ul>
             </div>

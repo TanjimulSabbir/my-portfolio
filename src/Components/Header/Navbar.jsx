b956fdc839351f49c400fce6f96a61b80b22c562
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 22:34:57 +0600

    navbar-hambarger-animation-add

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <div className="navbar flex item-center justify-end text-black z-50">
                 <div className="mid-lg:hidden">
                     <label tabIndex={0} onClick={() => setOpen(!open)} className="btn btn-outline UniversalMargin text-4xl border-none hover:bg-transparent text-black mid-lg:hidden">
-                        <p className={`${open && "transition duration-700 transform rotate-90 animate-spin"}`}>{open ? <RxCross1 /> : <RxHamburgerMenu />}</p>
+                        <p className={`${open && "transition duration-700 transform rotate-90"}`}>{open ? <RxCross1 /> : <RxHamburgerMenu />}</p>
                     </label>
                     <ul tabIndex={0} className={`${open || "hidden"} fixed inset-0 mt-20 px-6 py-10 shadow w-screen h-screen space-y-8 bg-white uppercase`}>
                         {Menu}

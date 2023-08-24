68321e1660fb28eb08c053c791dc8d74de94d875
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-24 19:04:18 +0600

    contact-page-designing

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,40 +1,53 @@
-import React from 'react'
-
+import { FiGithub, FiLinkedin, FiFacebook } from 'react-icons/fi';
+import { CiTwitter } from 'react-icons/ci';
+import { GrMedium } from 'react-icons/gr';
 const Contact = () => {
     return (
-        <div id='contact' className='mid-lg:w-3/5 py-24 mx-auto'>
+        <div id='contact' className='w-full mid-lg:w-3/5 pt-24 pb-14 mx-auto'>
             <div>
-                <div className='mb-20 text-center'>
+                <div className='mb-10 mid-lg:mb-20'>
                     <h1 className='Exo2_SanSer TitleText font-bold text-[#ccd6f6] mb-5'>Get In Touch</h1>
-                    <p className="text-custom-BgColor text-center rubik">I’m currently looking for any new opportunities. Need a front-end developer? Have any question or proposal? Or just want to say hello, I’ll try my best to get back to you!
+                    <p className="text-custom-BgColor  rubik">I’m currently looking for any new opportunities. Need a front-end developer? Have any question or proposal? Or just want to say hello, I’ll try my best to get back to you!
                     </p>
                 </div>
-                <div className='mid-lg:grid grid-rows-2 rubik text-custom-TextColor'>
-                    <div className='mid-lg:grid grid-cols-2 space-x-14'>
+                <div className='sm:grid grid-rows-2 rubik text-custom-TextColor '>
+                    <div className='sm:grid grid-cols-2 sm:space-x-14'>
                         <div className="mb-7 flex flex-col">
-                            <label htmlFor="fullName" className="text-xs">Enter name</label>
-                            <input type="text" name="fullname" id="fullName" 
-                                placeholder="Your Name"
-                                className="ContactPlaceHolder placeholder:rubik mt-5" />
+                            <label htmlFor="fullName" className="text-xs">Your name</label>
+                            <input type="text" name="fullname" id="fullName"
+                                placeholder="Entr Name"
+                                className="ContactPlaceHolder border-custom-TextColor placeholder:rubik mt-3" />
                         </div>
                         <div className="mb-7 flex flex-col">
                             <label htmlFor="email" className="text-xs">Email</label>
                             <input type="email" name="email" id="email" placeholder="Enter email address"
-                                className="ContactPlaceHolder placeholder:rubik mt-5" />
+                                className="ContactPlaceHolder border-custom-TextColor placeholder:rubik mt-3" />
                         </div>
                     </div>
                     <div className="mb-7 flex flex-col">
                         <label htmlFor="message" className="text-xs">Your Message</label>
-                        <textarea id="message" name="message" rows="3"
-                            placeholder="Write your message here"
-                            className="ContactPlaceHolder border-b resize-none placeholder:rubik"></textarea>
+                        <textarea id="message" name="message" rows=""
+                            placeholder=""
+                            className="ContactPlaceHolder border-custom-TextColor resize-none placeholder:rubik"></textarea>
                     </div>
                     <div data-aos="fade-down" data-aos-duration="700">
-                        <div className="mt-8 max-w-fit">
-                            <button type="submit" className="Btn">Send Message</button>
+                        <div className="mt-8 merina">
+                            <button type="submit" className="border border-custom-highLightTextColor p-4 text-custom-highLightTextColor rounded text-sm">Send Message</button>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className='mt-20 rubik'>
+                <h2>
+                    Feeling social? Find me on these online spaces too!
+                </h2>
+                <div className='flex items-center space-x-5 mt-5'>
+                <a className='list-none text-2xl text-white' href="/#"><FiGithub /> </a>
+                <a className='list-none text-2xl text-white' href="/#"><FiLinkedin /> </a>
+                <a className='list-none text-2xl text-white' href="/#"><FiFacebook /> </a>
+                <a className='list-none text-2xl text-white' href="/#"><CiTwitter /> </a>
+                <a className='list-none text-xl text-custom-highLightTextColor' href="/#"><GrMedium /> </a>
+            </div>
             </div>
         </div>
     )
--- src/Components/Pages/SocialLink/SocialLink.jsx
+++ src/Components/Pages/SocialLink/SocialLink.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
 import { FiGithub, FiLinkedin, FiFacebook } from 'react-icons/fi';
 import { CiTwitter } from 'react-icons/ci';
-import Email from '../Email/Email';
+import { GrMedium } from 'react-icons/gr';
+
 
 
 const SocialLink = () => {
     return (
-        <div className='hidden  mid-lg:fixed mid-lg:bottom-32 mid-lg:hidden'>
+        <div className='hidden mid-lg:fixed mid-lg:bottom-28 mid-lg:hidden Exo2_SanSer'>
             <div className='flex justify-center items-center flex-row mid-lg:flex-col space-x-8 mid-lg:space-x-0 mid-lg:space-y-8'>
                 <a className='list-none text-xl text-white' href="/#"><FiGithub /> </a>
                 <a className='list-none text-xl text-white' href="/#"><FiLinkedin /> </a>
                 <a className='list-none text-xl text-white' href="/#"><FiFacebook /> </a>
                 <a className='list-none text-xl text-white' href="/#"><CiTwitter /> </a>
+                <a className='list-none text-xl text-white' href="/#"><GrMedium /> </a>
             </div>
+            <span className="Social-Heading"></span>
         </div>
     );
 };

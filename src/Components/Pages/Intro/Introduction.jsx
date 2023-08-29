c425ba1d0218032cbfc045252090b466e220ebd1
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-29 08:44:12 +0600

    Bg-image-change-and-opacity-changes

--- src/Components/Pages/Intro/Introduction.jsx
+++ src/Components/Pages/Intro/Introduction.jsx
@@ -3,13 +3,13 @@ import Typewriter from "./TextAnimation";
 function Introduction() {
     const text = `I'm a skilled front-end developer with a passion for building dynamic and interactive websites. My expertise lies in harnessing the power of Javascript, React.js, Tailwind CSS, Redux, and other cutting-edge technologies to craft seamless web experiences that engage and captivate users.`;
     return (
-        <div className="h-screen flex items-center justify-center text-white bg-black bg-opacity-80">
+        <div className="min-h-screen flex items-center lg:items-start lg:pt-[10%] justify-center text-white bg-black bg-opacity-80">
             <div className="">
                 <div className="">
-                    <h2 className="text-4xl lg:text-[55px] xl:text-6xl uppercase font-lato">
-                        Hi, I'm Tanjimul Sabbir
+                    <h2 className="text-4xl lg:text-[55px] xl:text-6xl uppercase lato">
+                        Hi, I'm Tanjimul Islam Sabbir
                     </h2>
-                    <h1 className="mt-10 robotoSlab text-center text-[#dddddd]">Front-end Developer | Reactjs Developer</h1>
+                    <h1 className="mt-4 robotoSlab text-center text-[#b9bab9]">Front-end Developer | Reactjs Developer</h1>
                 </div>
             </div>
         </div>

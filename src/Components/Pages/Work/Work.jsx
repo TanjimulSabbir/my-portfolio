6e2d59be5fb2ed86386da0aae9775d3ee63c3169
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-29 06:48:17 +0600

    work-page-on-working

--- src/Components/Pages/Work/Work.jsx
+++ src/Components/Pages/Work/Work.jsx
@@ -12,10 +12,10 @@ const Work = () => {
 
             {/* Projects */}
             {
-                AllProjects?.Projects?.map(project => {
+                AllProjects?.Projects.map(project => {
                     const { projectName, Description, image, Technologies, Links } = project;
                     return (
-                        <> <div className="mid-lg:grid grid-cols-2 relative">
+                        <> <div className="mid-lg:grid grid-cols-2 relative mt-24">
                             {/* image part */}
                             <a href="">
                                 <div className="">
@@ -24,7 +24,7 @@ const Work = () => {
                                         {/* <div className="bg-cover bg-center rounded"
                                     style={{ backgroundImage: `url(${backgroundImage})`, height: '100%' }}>
                                 </div> */}
-                                        <img className="w-full h-full mid-lg:w-full mid-lg:h-[400px] rounded" src={image} alt="" srcset="" />
+                                        <img className="w-full h-full mid-lg:w-full mid-lg:h-full rounded" src={image} alt="" srcset="" />
                                         <div className="absolute inset-0 w-full h-full transition duration-500 cursor-pointer bg-[#0d2446] mid-lg:bg-green-500 bg-opacity-90 mid-lg:bg-opacity-30 hover:opacity-0"></div>
                                     </div>
                                 </div>
@@ -36,10 +36,10 @@ const Work = () => {
                                         <h2 className="mb-2 text-custom-highLightTextColor">Featured Project</h2>
                                         <h1 className="mb-8 text-custom-TitleColor text-3xl transition duration-500 cursor-pointer hover:text-custom-afterHoverTextColor Exo2_SanSer">{projectName}</h1>
                                         <div className="mid-lg:absolute mid-lg:w-[60%] mid-lg:h-[80px] right-0 bottom-[50%]">
-                                            <p className="mb-10 mid-lg:bg-[#0d2446] text-base mid-lg:text-sm mid-lg:p-4 lg:py-5 lg:px-8 rounded text-custom-TextColor mid-lg:shadow-2xl z-20 robotoMono">{Description}</p>
+                                            <p className="mb-10 mid-lg:bg-[#0d2446] text-base mid-lg:text-sm mid-lg:p-4 lg:py-5 lg:px-8 rounded text-custom-TextColor mid-lg:shadow-2xl z-20 rubik text-justify">{Description}</p>
 
 
-                                            <p className="mb-4 robotoMono"><small>{Technologies.map(technology => technology)}</small></p>
+                                            <p className="mb-4 robotoMono space-x-1 cursor-pointer text-custom-highLightTextColor">{Technologies.map(technology =><small key={technology}> {technology}</small>)}</p>
                                             <div className="flex items-center space-x-8 text-xl mid-lg:justify-end">
                                                 <a className="LinkHover" href={Links.github}><FiGithub /> </a>
                                                 <a className="LinkHover" href={Links.live}> <LuExternalLink /></a>

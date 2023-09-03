4da501c03f99c320326df58774e9522600c11779
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-04 03:30:39 +0600

    intro-page-items-center-omit-for-small

--- src/Components/Pages/Intro/Introduction.jsx
+++ src/Components/Pages/Intro/Introduction.jsx
@@ -3,19 +3,19 @@ import Typewriter from "./TextAnimation";
 function Introduction() {
     const text = `I'm a skilled front-end developer with a passion for building dynamic and interactive websites. My expertise lies in harnessing the power of Javascript, React.js, Tailwind CSS, Redux, and other cutting-edge technologies to craft seamless web experiences that engage and captivate users.`;
     return (
-        <div className="h-screen w-full bg-[#020202] bg-opacity-80 text-[#35a55e] ">
-            <div className="IntroMargin w-1/2 h-full mx-auto flex items-center justify-center z-10 ">
-                <div className="-mt-20">
+        <div className="UniversalPadding h-screen w-full bg-[#020202] bg-opacity-80 text-[#35a55e] ">
+            <div className="lg:w-1/2 h-full flex pt-24 md:pt-0 md:items-center justify-center  z-10">
+                <div className="md:-mt-28">
                     <h4 className='mb-2 lato'>
                         Hi, my name is
                     </h4>
-                    <h2 className='Exo2  text-4xl lg:text-[55px] xl:text-6xl font-extrabold uppercase whitespace-nowrap'>
+                    <h2 className='Exo2 text-3xl md:text-4xl lg:text-[55px] xl:text-6xl font-extrabold uppercase whitespace-nowrap'>
                         Tanjimul Islam Sabbir
                     </h2>
 
                     <h1 className="mt-2 lato">Front-end Developer | Reactjs Developer</h1>
                     {/* <Typewriter text={text} /> */}
-                    <p className="mt-7 lato">{text}</p>
+                    <p className="mt-7 lato pl-0">{text}</p>
                     <button className="mt-6 uppercase border-2 rounded border-[#238748] px-14 py-4">get in touch</button>
                 </div>
             </div>

5fbf15e69d98c96380dbdbae902c4357cbdac1d6
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-04 02:47:57 +0600

    intro-page-mixing-old

--- src/Components/Pages/Intro/Introduction.jsx
+++ src/Components/Pages/Intro/Introduction.jsx
@@ -3,25 +3,26 @@ import Typewriter from "./TextAnimation";
 function Introduction() {
     const text = `I'm a skilled front-end developer with a passion for building dynamic and interactive websites. My expertise lies in harnessing the power of Javascript, React.js, Tailwind CSS, Redux, and other cutting-edge technologies to craft seamless web experiences that engage and captivate users.`;
     return (
-        <div className="min-h-screen w-full bg-[#21827c] bg-opacity-70 lg:flex justify-center text-black dark:text-black relative">
-            <div className="IntroMargin flex-1 flex pt-10 sm:pt-24 lg:pt-0 items-center lg:justify-center z-10">
+        <div className="h-screen w-full bg-black bg-opacity-80 text-[#35a55e]">
+            <div className="IntroMargin w-1/2 h-full mx-auto flex items-center justify-center z-10">
                 <div className="">
                     <h4 className='mb-2'>
                         Hi, my name is
                     </h4>
-                    <h2 className='Exo2_SanSer text-4xl lg:text-[55px] xl:text-6xl font-extrabold uppercase whitespace-nowrap'>
+                    <h2 className='Exo2 text-[#35a55e] text-4xl lg:text-[55px] xl:text-6xl font-extrabold uppercase whitespace-nowrap'>
                         Tanjimul Islam Sabbir
                     </h2>
 
                     <h1 className="mt-2 robotoMono">Front-end Developer| Reactjs Developer</h1>
-                    <Typewriter text={text} />
-                    <button className="mt-6 uppercase border border-black px-14 py-4">get in touch</button>
+                    {/* <Typewriter text={text} /> */}
+                    <p className="mt-7">{text}</p>
+                    <button className="mt-6 uppercase border border-[#35a55e] px-14 py-4">get in touch</button>
                 </div>
             </div>
 
-            <div className="flex-1 mt-10 lg:mt-0 flex items-end justify-center">
-                {/* <img className="w-[350px] sm:w-[435px] lg:w-[550px]" src={intro} alt="TanjimulSabbir" /> */}
-            </div>
+            {/* <div className="flex-1 mt-10 lg:mt-0 flex items-end justify-center">
+                <img className="w-[350px] sm:w-[435px] lg:w-[550px]" src={intro} alt="TanjimulSabbir" />
+            </div> */}
         </div>
     )
 }

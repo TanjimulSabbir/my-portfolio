bd364e58bcb07f215f9bcc5aa30765c1ca097a1d
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-09-04 02:57:03 +0600

    intro-page-mixing-old

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <div className='bg-black bg-opacity-80'>
+        <div className='bg-black bg-opacity-90'>
             <Navbar></Navbar>
         </div>
     )

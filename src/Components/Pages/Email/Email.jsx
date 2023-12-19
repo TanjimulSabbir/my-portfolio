8c2d8a3e3956b7ce550fe2388b7093ace8db5b23
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-12-20 02:02:47 +0600

    email updated

--- src/Components/Pages/Email/Email.jsx
+++ src/Components/Pages/Email/Email.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const Email = () => {
     return (
         <div className='hidden mid-lg:fixed justify-center mt-4 flex-row mid-lg:bottom-60 mid-lg:right-0 w-full mid-lg:inline-block mid-lg:transform mid-lg:rotate-90 skew-x-12 transform-origin mid-lg:w-10 mid-lg:h-1 LinkHover'>
-            <a href="mailto:tanjimulislamsabbir02@gmail.com?subject=Say%20Hello!&body=Hi,Tanjimul Sabbir. I wanna to know about your projects." className='text-[11px] text-[#ccd6f6 tracking-[2px]'>tanjimulsabbir.dev@gmail.com</a>
+            <a href="mailto:tanjimulsabbir.dev@gmail.com?subject=Say%20Hello!&body=Hi,Tanjimul Sabbir. I wanna to know about your projects." className='text-[11px] text-[#ccd6f6 tracking-[2px]'>tanjimulsabbir.dev@gmail.com</a>
         </div>
     );
 };
